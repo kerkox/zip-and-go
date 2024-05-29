756d4cfd5bc11a7fbe69303cb1fcd52a58e92000
Juan Llano <juan.llano@gorillalogic.com>
2024-05-29 09:47:01 -0500

    add final changes

--- generator.go
+++ generator.go
@@ -3,52 +3,80 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"encoding/json"
-	"io/ioutil"
 	"io"
 	"strings"
+	"time"
 	"archive/zip"
+	"github.com/kardianos/osext"
+	"github.com/schollz/progressbar/v3"
 )
+
+type SpecificFiles struct {
+	FilePath string `json:"filePath"`
+	GenerationScript []string `json:"generationScript"`
+}
 
 type Rules struct {
 	Directories []string `json:"directories"`
 	ExcludedFiles []string `json:"excludedFiles"`
 	Destination string `json:"destination"` 
+	SpecificFiles map[string][]SpecificFiles `json:"specificFiles"`
 }
 
 func main() {
-	fmt.Println("Hello, world.")
-	buildData := getRules()
+	folderPath, pathErr := osext.ExecutableFolder()
+	if pathErr != nil {
+		panic(pathErr)
+	}
+	fmt.Println("Running on this folder: ", folderPath)
+	buildData := getRules(folderPath)
 	excludedFiles := make(map[string]bool)
+	now := time.Now()
+	cTime := now.Format("2006-01-02 15:04:05")
 	for _, num := range buildData.ExcludedFiles {
     excludedFiles[num] = true
 	}
-	fmt.Println(excludedFiles)
+	fmt.Println("running directories")
 	for i:=0; i<len(buildData.Directories); i++ {
-		fmt.Println(buildData.Directories[i])
-		filesToCompres:=listDirectoryFiles(buildData.Directories[i], excludedFiles)
-		fmt.Println(filesToCompres)
+		currentFolder:=""
+		zipName:=""
+		if dirExists(folderPath+"/"+buildData.Directories[i]) {
+			currentFolder=folderPath+"/"
+			zipName=folderPath+"/"+buildData.Directories[i]
+		} else {
+			currentFolder="./"
+			zipName=buildData.Directories[i]
+		}
+		fmt.Println("Validating specific directory ",buildData.SpecificFiles[buildData.Directories[i]])
+		//Not needed for the MVP
+		//docker compose can generate the metadata missing file
+		//validateAndGenerateRequiredFiles(buildData.SpecificFiles[buildData.Directories[i]])
+		fmt.Println("List files in ",buildData.Directories[i])
+		filesToCompres:=listDirectoryFiles(currentFolder,buildData.Directories[i], excludedFiles)
+		//fmt.Println(filesToCompres)
 		//For the MVP loop again the listed files and compress them
 		//For the Version 2, compress them in parallel as they are listed
 		fmt.Println("creating zip archive...")
-		fmt.Println(strings.TrimLeft(buildData.Directories[i], "./")+".zip")
-    archive, err := os.Create(strings.TrimLeft(buildData.Directories[i], "./")+".zip")
+		fmt.Println(buildData.Directories[i]+"-"+cTime+".zip")
+    archive, err := os.Create(zipName+"-"+cTime+".zip")
     if err != nil {
         panic(err)
     }
 		defer archive.Close()
 		zipWriter := zip.NewWriter(archive)
+		//initialize bar
+		bar := progressbar.Default(int64(len(filesToCompres)))
 		//Write the files on the corresponding zip file
 		for j:=0; j<len(filesToCompres); j++ {
-			fmt.Println(filesToCompres[j])
-			fmt.Println("Opening file: " + filesToCompres[j])
-			f, err := os.Open(filesToCompres[j])
+			// fmt.Println(filesToCompres[j])
+			// fmt.Println("Opening file: " + filesToCompres[j])
+			f, err := os.Open(currentFolder+filesToCompres[j])
 			if err != nil {
 				fmt.Println(err)
 			}
 			defer f.Close()
-
-			fmt.Println("Writing file in the zip archive")
 			w, err := zipWriter.Create(filesToCompres[j])
 			if err != nil {
 				fmt.Println(err)
@@ -57,6 +85,7 @@ func main() {
 				fmt.Println(err)
 				panic(err)
 			}
+			bar.Add(1)
 		}
 		fmt.Println("Close Zip Archive...")
 		zipWriter.Close()
@@ -64,26 +93,33 @@ func main() {
 	}
 }
 
-func getRules() Rules {
-	jsonFile, err := os.Open("rules.json")
+func getRules(filePath string) Rules {
+	fmt.Println("Prepare to open rules...")
+	jsonFile, err := os.Open(filePath+"/rules.json")
 	// if we os.Open returns an error then handle it
 	if err != nil {
 			fmt.Println(err)
+			newjsonFile, newerr := os.Open("./rules.json")
+			if newerr != nil { 
+				panic(newerr)
+			}
+			jsonFile = newjsonFile
 	}
 	fmt.Println("Successfully Opened rules.json")
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
-	fileInBytes, _ := ioutil.ReadAll(jsonFile)
+	fileInBytes, _ := io.ReadAll(jsonFile)
 	var buildData Rules
 	json.Unmarshal(fileInBytes, &buildData)
 	return buildData
 }
 
-func listDirectoryFiles(directory string, excludedFiles map[string]bool) []string{
-	files, err := ioutil.ReadDir(directory)
+func listDirectoryFiles(currentPath string,directory string, excludedFiles map[string]bool) []string{
+	files, err := os.ReadDir(currentPath+directory)
 	if err != nil {
 			fmt.Println(err)
+			panic(err)
 	}
 	var res []string
 
@@ -92,11 +128,39 @@ func listDirectoryFiles(directory string, excludedFiles map[string]bool) []strin
 			fmt.Println("Excluded file: " + file.Name())
 		} else {
 			if file.IsDir() {
-				innerDir := listDirectoryFiles(directory + "/" + file.Name(), excludedFiles)
+				innerDir := listDirectoryFiles(currentPath,directory + "/" + file.Name(), excludedFiles)
 				res = append(res, innerDir...)
 			} else {
 				res = append(res, directory + "/" + file.Name())
 			}
+		}
+		
+	}
+	return res
+}
+
+func dirExists(path string) (bool) {
+	_, err := os.Stat(path)
+	if err == nil { return true }
+	if os.IsNotExist(err) { return false }
+	panic(err)
+}
+
+func validateAndGenerateRequiredFiles(files []SpecificFiles) bool {
+	res:=true
+	for _, num := range files {
+		fmt.Println("reading ",num.FilePath)
+    _, err := os.ReadDir(num.FilePath)
+		if err != nil {
+			fmt.Println("file not found")
+			fmt.Print("executing command:",strings.Join(num.GenerationScript[:], ","))
+			cmd := exec.Command(num.GenerationScript[0], num.GenerationScript[1:]...)
+			stdout, execErr := cmd.Output()
+			if execErr != nil {
+					fmt.Println(err.Error())
+					res=false
+			}
+			fmt.Println(string(stdout))
 		}
 		
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module go-build-generator
 
 go 1.22.1
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v3 v3.14.3 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+)
